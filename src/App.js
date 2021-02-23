929f5e79f0989b8a1eaf09635f4133cfbbd9ef9e
Say-young <seyoung990525@gmail.com>
2021-02-23 15:52:56 +0900

    기본 라우팅 작업

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
+import AllResultPage from './pages/AllResultPage';
+import DeveloperPage from './pages/DeveloperPage';
 
 function App() {
 
   return (
-    <LandingPage />
+    <Switch>
+      <Route
+        exact
+        path="/"
+        render={props => (
+          <LandingPage />
+        )}
+      ></Route>
+      <Route
+        exact
+        path="/all"
+        render={props => <AllResultPage props={props} />}
+      ></Route>
+      <Route
+        exact
+        path="/makers"
+        render={props => <DeveloperPage props={props} />}
+      ></Route>
+    </Switch>
   );
 
 }
---
+++ src/pages/AllResultPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AllResultPage() {
+
+    return (
+        <>
+            야호
+        </>
+    );
+}
+
+export default AllResultPage;
